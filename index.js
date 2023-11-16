58df0105545201a983e4a3346fd8181f934dce79
Tanmay Gupta <43621750+tanmayGypt@users.noreply.github.com>
2023-11-16 22:50:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+const { connect } = require("http2");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Socket.io
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    console.log(message);
+    io.emit("message", message);
+  });
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
+
+server.listen(9000, () => console.log(`Server Started at PORT:9000`));
